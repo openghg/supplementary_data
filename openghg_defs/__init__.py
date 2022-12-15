c455e004ee40bc41fe01230b2345081bf9c43755
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2022-12-15 17:41:45 +0000

    Adding type hinting for mypy to use

--- openghg_defs/__init__.py
+++ openghg_defs/__init__.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 module_root = Path(__file__).resolve().parent
 
-data_path = module_root / "data"
+data_path: Path = module_root / "data"
 
-site_info_file = data_path / "site_info.json"
-species_info_file = data_path / "species_info.json"
-domain_info_file = data_path / "domain_info.json"
+site_info_file: Path = data_path / "site_info.json"
+species_info_file: Path = data_path / "species_info.json"
+domain_info_file: Path = data_path / "domain_info.json"
 
